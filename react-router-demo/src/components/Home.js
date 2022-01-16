dafe73d1dcafefcf726c8f1318019cf052ed53a6
spanda <santanu.panda@gcp.lowes.com>
2022-01-16 23:35:28 +0530

    No Match Route

--- react-router-demo/src/components/Home.js
+++ react-router-demo/src/components/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Home Page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      Home Page
+      <button onClick={() => navigate("order-summary")}>Place Order</button>
+      <button onClick={() => navigate("order-summary", { replace: true })}>
+        Place Order 1
+      </button>
+    </div>
+  );
 };
