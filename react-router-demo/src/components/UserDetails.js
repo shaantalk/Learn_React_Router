ecb0eb9a4e7aecacea1a0fde05ecbd405b9d6f24
spanda <santanu.panda@gcp.lowes.com>
2022-01-31 01:26:45 +0530

    URL Params

--- react-router-demo/src/components/UserDetails.js
+++ react-router-demo/src/components/UserDetails.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
-  return <div>Details about users</div>;
+  const params = useParams();
+  const userId = params.userId;
+  return <div>Details about users {userId}</div>;
 };
