f8bbb1936b2a863ea6f182643c059e2eba11bd3f
spanda <santanu.panda@gcp.lowes.com>
2022-01-31 00:58:15 +0530

    Dynamic Route

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
+import { Admin } from "./components/Admin";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
@@ -7,6 +8,8 @@ import { NewProducts } from "./components/NewProducts";
 import { NoMatch } from "./components/NoMatch";
 import { OrderSummary } from "./components/OrderSummary";
 import { Products } from "./components/Products";
+import { UserDetails } from "./components/UserDetails";
+import { Users } from "./components/Users";
 
 function App() {
   return (
@@ -17,11 +20,18 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
+        {/* Nested Route */}
         <Route path='products' element={<Products />}>
+          {/* Index Route */}
           <Route index element={<FeaturedProducts />} />
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProducts />} />
         </Route>
+        {/* Dynamic Route */}
+        <Route path='users' element={<Users />} />
+        <Route path='users/:userId' element={<UserDetails />} />
+        <Route path='users/admin' element={<Admin />} />
+        {/* No Match Route */}
         <Route path='*' element={<NoMatch />} />
       </Routes>
     </>
